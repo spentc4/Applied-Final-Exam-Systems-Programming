9bfe45324e9fcf42336df235c3465386ced2e9cd
spentc4 <50633935+spentc4@users.noreply.github.com>
2020-12-02 20:59:26 -0500

    Add files via upload

--- chap8prob11/chap8prob11/main.cpp
+++ chap8prob11/chap8prob11/main.cpp
@@ -8,37 +8,35 @@
 
 #include <iostream>
 #include <fstream>
-#include <string.h>
 using namespace std;
-const int nRow = 7;
-const int nCol = 10;
+const int NROWS = 7;
+const int NCOLS = 10;
 int main(int argc, const char * argv[]) {
     // insert code here...
-    double totalPower = 0;
-    double avgPower;
-    double powerOutput[nRow][nCol];
-    unsigned int countDays = 0;
-    int row,col;
-    
-    ifstream power;
-    power.open("power1.dat");
-    if(power){
-        for (row=0; row < nRow; row++){
-            for (col = 0; col < nCol; col++){
-            power >> powerOutput[row][col];
-            totalPower += powerOutput[row][col];
+    float total = 0;
+    float average;
+    float output[NROWS][NCOLS];
+    unsigned int counter;
+    int row, col;
+    ifstream file;
+    file.open("power1.dat");
+    if(file){
+        for(row=0;row<NROWS;row++){
+            for(col=0;col<NCOLS;col++){
+                file>>output[row][col];
+                total+=output[row][col];
             }
         }
-    } else {}
-    
-    power.close();
-    avgPower = totalPower/(nRow*nCol);
-    for (row=0; row < nRow; row++){
-        for (col=0;col < nCol; col++){
-            if (powerOutput[row][col] > avgPower)
-            countDays++;
+    }
+    file.close();
+    average = total/(NROWS*NCOLS);
+    for(row=0;row<NROWS;row++){
+        for(col=0;col<NCOLS;col++){
+            if(output[row][col]>average)
+                counter++;
         }
     }
-    cout << "power output: " << avgPower << " megaWatts " << "days with power output greater than average " << countDays << endl;
+    cout<<"the average power output was "<<average<<" megawatts."<<endl;
+    cout<<"the amount of days over the average was "<<counter<<" days."<<endl;
     return 0;
 }
